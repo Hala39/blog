3c42e0d93b6f68b2c1cf5255810d3c3960f4dc83
Hala Taleb <hala.taleb.39@gmail.com>
2021-12-21 00:53:54 +0200

    done with navigation bar

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
+import { fromEvent, ReplaySubject, takeUntil } from 'rxjs';
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
-  styleUrls: ['./navigation-bar.component.scss']
+  styleUrls: ['./navigation-bar.component.scss'],
+  animations: [
+    
+  ]
 })
 export class NavigationBarComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    this.displayNavigationBar = window.innerWidth > 768;
+  }
+
+  displayNavigationBar: boolean;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.displayNavigationBar = event.target.innerWidth > 768;
   }
 
 }
